67533cc380242b7f675ba559fc1f457432d6fbfa
Vishal Regmi <regmibishal42@pop-os.localdomain>
2021-09-29 13:55:47 +0545

    Learning RTK query and react charts implementation

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,32 @@
+
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+
+const cryptoApiHeaders = {
+        'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+        'x-rapidapi-key': process.env.REACT_APP_CRYPTO_API_KEY
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url:url,headers:cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl:baseUrl}),
+    endpoints:(builder) =>({
+        getCryptos:builder.query({
+            query: (count)=> createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails:builder.query({
+            query:(coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query:({coinId,timePeriod}) => createRequest(`/coin/${coinId}/history/${timePeriod}`),
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,useGetCryptoDetailsQuery,useGetCryptoHistoryQuery
+} = cryptoApi;
